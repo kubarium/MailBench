96f443b84bfb262a3278ace8457d206c52d875d8
kubarium <iam@denizkumsal.com>
2017-02-09 17:41:55 -0500

    implements preview panel

---
+++ src/Preview.js
@@ -0,0 +1,79 @@
+import {
+    Button,
+    ButtonGroup,
+    ButtonToolbar,
+    DropdownButton,
+    Form,
+    FormControl,
+    FormGroup,
+    InputGroup,
+    MenuItem,
+    Nav,
+    NavDropdown,
+    NavItem,
+    Navbar,
+    Panel,
+} from 'react-bootstrap'
+import { FaBinoculars, FaClipboard, FaEnvelope, FaFloppyO } from 'react-icons/lib/fa'
+import React, { Component } from 'react';
+
+import Emphasize from './Emphasize'
+
+const previewOperations = (
+    <Navbar>
+        <Navbar.Collapse>
+            <Navbar.Form>
+                <ButtonGroup>
+                    <Button><FaBinoculars/> <Emphasize text="Preview"/></Button>
+                    <Button><FaEnvelope/> <Emphasize text="Send"/></Button>
+                    <Button><FaClipboard/> <Emphasize text="Copy to Clipboard"/></Button>
+                </ButtonGroup>
+                <ButtonGroup>
+                    <InputGroup>
+                        <DropdownButton
+                            componentClass={InputGroup.Button}
+                            title="Width Presets"
+                            id="dropdown-no-caret">
+                            <MenuItem eventKey="1">Action</MenuItem>
+                            <MenuItem eventKey="2">Another action</MenuItem>
+                            <MenuItem eventKey="3">Something else here</MenuItem>
+                            <MenuItem divider/>
+                            <MenuItem eventKey="4">Separated link</MenuItem>
+                        </DropdownButton>
+                        <FormControl type="text"/>
+                        <InputGroup.Button>
+                            <Button><FaFloppyO/></Button>
+                        </InputGroup.Button>
+                    </InputGroup>
+                    <InputGroup>
+                        <InputGroup.Addon>Height</InputGroup.Addon>
+                        <FormControl type="text"/>
+                    </InputGroup>
+                </ButtonGroup>
+            </Navbar.Form>
+        </Navbar.Collapse>
+    </Navbar>
+
+)
+
+const previewHeader = (
+    <h2>Preview and Send</h2>
+)
+
+export default class Preview extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <Panel id="Preview" header={previewHeader} footer={previewOperations}>
+                {previewOperations}
+                fuck me
+            </Panel >
+        )
+
+    }
+}
