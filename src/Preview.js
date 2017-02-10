423bef57f4fe6b85debdfc2aa805fc9bf7701b77
Kumsal Obuz <iam@denizkumsal.com>
2017-02-10 17:46:29 -0500

    serapates preview display area from preview operations

--- src/Preview.js
+++ src/Preview.js
@@ -1,63 +1,12 @@
-import {
-    Button,
-    ButtonGroup,
-    ButtonToolbar,
-    DropdownButton,
-    Form,
-    FormControl,
-    FormGroup,
-    InputGroup,
-    MenuItem,
-    Nav,
-    NavDropdown,
-    NavItem,
-    Navbar,
-    Panel,
-} from 'react-bootstrap'
-import { FaBinoculars, FaClipboard, FaEnvelope, FaFloppyO } from 'react-icons/lib/fa'
 import React, { Component } from 'react';
 
-import Emphasize from './Emphasize'
-import Space from 'react-nbsp'
-const previewOperations = (
-    <Navbar>
-        <Navbar.Collapse>
-            <Navbar.Form>
-                <ButtonGroup>
-                    <Button><FaBinoculars/> <Emphasize text="Preview"/></Button>
-                    <Button><FaEnvelope/> <Emphasize text="Send"/></Button>
-                    <Button><FaClipboard/> <Emphasize text="Copy to Clipboard"/></Button>
-                </ButtonGroup><Space/>
-                <ButtonGroup>
-                    <InputGroup>
-                        <DropdownButton
-                            componentClass={InputGroup.Button}
-                            title="Width Presets"
-                            id="dropdown-no-caret">
-                            <MenuItem eventKey="1">Action</MenuItem>
-                            <MenuItem eventKey="2">Another action</MenuItem>
-                            <MenuItem eventKey="3">Something else here</MenuItem>
-                            <MenuItem divider/>
-                            <MenuItem eventKey="4">Separated link</MenuItem>
-                        </DropdownButton>
-                        <FormControl type="text"/>
-                        <InputGroup.Button>
-                            <Button><FaFloppyO/></Button>
-                        </InputGroup.Button>
-                    </InputGroup>
-                    <InputGroup>
-                        <InputGroup.Addon>Height</InputGroup.Addon>
-                        <FormControl type="text"/>
-                    </InputGroup>
-                </ButtonGroup>
-            </Navbar.Form>
-        </Navbar.Collapse>
-    </Navbar>
-
-)
+import {
+Panel,
+} from 'react-bootstrap'
+import PreviewOperations from './PreviewOperations'
 
 const previewHeader = (
-    <h2>Preview and Send</h2>
+<h2>Preview and Send</h2>
 )
 
 export default class Preview extends Component {
@@ -66,13 +15,16 @@ export default class Preview extends Component {
 
         this.state = {}
     }
+    updateFrameWidth = (width) => this.refs.frame.style.width = width
+
+    updateFrameHeight = (event) => this.refs.frame.style.height = isNaN(parseInt(event.target.value, 10)) ? "1000px" : event.target.value + "px"
 
     render() {
         return (
-            <Panel id="Preview" header={previewHeader} footer={previewOperations}>
-                {previewOperations}
-                fuck me
-            </Panel >
+            <Panel id="Preview" header={ previewHeader } footer={ <PreviewOperations/> }>
+              <PreviewOperations updateFrameWidth={ this.updateFrameWidth } updateFrameHeight={ this.updateFrameHeight } />
+              <iframe ref="frame"></iframe>
+            </Panel>
         )
 
     }
