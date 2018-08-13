91a7fb7b462fec271c8aec000fa38ca46ac75bb9
kubarium <iam@denizkumsal.com>
2018-08-12 23:44:08 -0400

    adds toggle to Settings sections

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -4,8 +4,10 @@
       <div class="box">
 
         <div class="field">
-          <label class="label has-text-info has-background-light">Litmus Checklist</label>
-          <div class="field has-addons">
+          <label class="label has-text-info has-background-light" @click="toggle('litmus')" @toggle="toggle('litmus')">Litmus Checklist
+            <toggle :toggled="section==='litmus'" />
+          </label>
+          <div class="field has-addons" v-show="section==='litmus'">
 
             <p class="control is-expanded">
               <input class="input" type="email" placeholder="Your Litmus test address to create a new Checklist" v-model="settings.litmus">
@@ -27,82 +29,87 @@
       </div>
       <div class="box">
         <div class="field">
-          <label class="label has-text-info has-background-light">Mail Server</label>
-          <div class="field is-horizontal">
-            <div class="field-label">
-              <label class="label">
-                Host/Port
-              </label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <p class="control is-expanded has-icons-left">
-                  <input class="input" type="text" placeholder="mail.domain.com" v-model="settings.mail.host">
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-server"></i>
-                  </span>
-                </p>
+          <label class="label has-text-info has-background-light" @click="toggle('server')" @toggle="toggle('server')">Mail Server
+            <toggle :toggled="section==='server'" />
+          </label>
+          <div v-show="section==='server'">
+
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label">
+                  Host/Port
+                </label>
               </div>
-              <div class="field is-narrow">
-                <p class="control has-icons-left">
-                  <input class="input" type="number" min="0" placeholder="22" v-model="settings.mail.port">
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-globe"></i>
-                  </span>
-                </p>
+              <div class="field-body">
+                <div class="field">
+                  <p class="control is-expanded has-icons-left">
+                    <input class="input" type="text" placeholder="mail.domain.com" v-model="settings.mail.host">
+                    <span class="icon is-small is-left">
+                      <i class="fas fa-server"></i>
+                    </span>
+                  </p>
+                </div>
+                <div class="field is-narrow">
+                  <p class="control has-icons-left">
+                    <input class="input" type="number" min="0" placeholder="22" v-model="settings.mail.port">
+                    <span class="icon is-small is-left">
+                      <i class="fas fa-globe"></i>
+                    </span>
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label">
-              <label class="label">
-                Encryption
-              </label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control is-expanded">
-                  <div class="select is-fullwidth">
-                    <select @change="$store.commit('changeEncryption',$event.target.value)">
-                      <option value="22">Unencrpted</option>
-                      <option value="465">SSL/TLS</option>
-                      <option value="587">STARTTLS</option>
-                    </select>
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label">
+                  Encryption
+                </label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <div class="control is-expanded">
+                    <div class="select is-fullwidth">
+                      <select @change="$store.commit('changeEncryption',$event.target.value)">
+                        <option value="22">Unencrpted</option>
+                        <option value="465">SSL/TLS</option>
+                        <option value="587">STARTTLS</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div class="field is-vertically-centered is-narrow">
+                  <div class="control is-expanded">
+                    <label class="checkbox is-fullwidth">
+                      Authenticate
+                      <input type="checkbox" v-model="settings.mail.auth" />
+                    </label>
                   </div>
                 </div>
               </div>
-              <div class="field is-vertically-centered is-narrow">
-                <div class="control is-expanded">
-                  <label class="checkbox is-fullwidth">
-                    Authenticate
-                    <input type="checkbox" v-model="settings.mail.auth" />
-                  </label>
+            </div>
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label">
+                  Credentials
+                </label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <p class="control is-expanded has-icons-left">
+                    <input class="input" type="text" placeholder="Username/Email" v-model="settings.mail.username">
+                    <span class="icon is-small is-left">
+                      <i class="fas fa-user"></i>
+                    </span>
+                  </p>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label">
-              <label class="label">
-                Credentials
-              </label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <p class="control is-expanded has-icons-left">
-                  <input class="input" type="text" placeholder="Username/Email" v-model="settings.mail.username">
-                  <span class="icon is-small is-left">
-                    <i class="fas fa-user"></i>
-                  </span>
-                </p>
-              </div>
-              <div class="field">
-                <p class="control is-expanded has-icons-left">
-                  <input class="input" type="password" placeholder="Password" v-model="settings.mail.password">
-                  <span class=" icon is-small is-left">
-                    <i class="fas fa-key"></i>
-                  </span>
-                </p>
+                <div class="field">
+                  <p class="control is-expanded has-icons-left">
+                    <input class="input" type="password" placeholder="Password" v-model="settings.mail.password">
+                    <span class=" icon is-small is-left">
+                      <i class="fas fa-key"></i>
+                    </span>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
@@ -111,8 +118,10 @@
       <div class="box">
 
         <div class="field">
-          <label class="label has-text-info has-background-light">Email Variables</label>
-          <div class="field has-addons">
+          <label class="label has-text-info has-background-light" @click="toggle('email')" @toggle="toggle('email')">Email Variables
+            <toggle :toggled="section==='email'" />
+          </label>
+          <div class="field has-addons" v-show="section==='email'">
 
             <p class="control is-expanded">
               <input class="input" type="email" placeholder="Your Litmus test address to create a new Checklist" v-model="settings.litmus">
@@ -138,7 +147,9 @@
 
 <script>
 import Modal from "@/components/Modal";
+import Toggle from "@/components/Toggle";
 import { mapState } from "vuex";
+
 export default {
   name: "Settings",
   mounted() {
@@ -148,10 +159,14 @@ export default {
   },
   data() {
     return {
+      section: "litmus",
       hasUnsavedChanges: false
     };
   },
   methods: {
+    toggle(section) {
+      this.section = section;
+    },
     saveChanges() {
       this.hasUnsavedChanges = false;
       this.$store.commit("updateSettings", this.settings);
@@ -165,7 +180,8 @@ export default {
     }
   },
   components: {
-    Modal
+    Modal,
+    Toggle
   }
 };
 </script>
@@ -184,6 +200,7 @@ input[type="number"] {
 label:not(:last-child) {
   padding: 0.25rem;
   border-radius: 3px;
+  cursor: pointer;
 }
 </style>
 
---
+++ src/components/Toggle.vue
@@ -0,0 +1,15 @@
+<template>
+    <span class="icon is-pulled-right" @click="toggle">
+        <i class="fas fa-angle-right" :class="{'fa-angle-down':toggled}"></i>
+    </span>
+</template>
+<script>
+export default {
+  props: ["toggled", "section"],
+  methods: {
+    toggle() {
+      this.$el.parentElement.dispatchEvent(new Event("toggle"));
+    }
+  }
+};
+</script>
