582cf53f3c3e49545fa5aee5ac7d2d15aa48ea4b
kubarium <iam@denizkumsal.com>
2018-08-16 21:53:33 -0400

    introduces addition of a new template modal

---
+++ src/components/AddTemplate.vue
@@ -0,0 +1,45 @@
+<template>
+    <modal title="Add New Template" parent="addTemplate">
+        <div slot="body">asdasd</div>
+    </modal>
+</template>
+
+<script>
+import Modal from "@/components/Modal";
+import Toggle from "@/components/Toggle";
+
+export default {
+  name: "AddTemplate",
+  mounted() {
+    ["change", "keyup"].forEach(event => {
+      this.$el.addEventListener(event, () => (this.hasUnsavedChanges = true));
+    });
+  },
+  data() {
+    return {
+      section: "recipients",
+      hasUnsavedChanges: false
+    };
+  },
+  methods: {
+    toggle(section) {
+      this.section = section;
+    },
+    saveChanges() {
+      this.hasUnsavedChanges = false;
+      this.$store.commit("updateSettings", this.settings);
+    }
+  },
+  computed: {
+    settings: {
+      get() {
+        return this.$store.state.settings;
+      }
+    }
+  },
+  components: {
+    Modal,
+    Toggle
+  }
+};
+</script>
